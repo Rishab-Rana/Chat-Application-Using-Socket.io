5614f19e16af36ec75bee0b5c0cfc88fbd2a1bbf
Rishab-Rana <rishabrana228@gmail.com>
2023-11-20 17:35:39 +0530

    added total count of the client functality in the aoolication

--- app.js
+++ app.js
@@ -5,4 +5,23 @@ const app = express()
 const PORT = process.env.PORT || 4000
 const server = app.listen(PORT,()=> console.log(`🗨️  server is running on port ${PORT}`))
 
+const io = require("socket.io")(server)
+
 app.use(express.static(path.join(__dirname,'public')))
+
+let socketConnected = new Set()
+
+io.on('connection',onConnected)
+
+function onConnected(socket){
+    console.log(socket.id)
+    socketConnected.add(socket.id)
+
+    io.emit('clients-total',socketConnected.size)
+
+    socket.on('disconnect',()=>{
+        console.log('socket is disconnected: ',socket.id)
+        socketConnected.delete(socket.id)
+        io.emit('clients-total',socketConnected.size)
+    })
+}
